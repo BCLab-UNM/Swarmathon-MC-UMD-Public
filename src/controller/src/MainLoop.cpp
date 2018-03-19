60c19bf714e4be22f9ae886b184bbd00d29b52b3
Denys Fedorchuk <dfedorchuk@eastbanctech.com>
2018-03-19 11:54:30 -0400

    Set search behavior as starting behavior

--- src/controller/src/MainLoop.cpp
+++ src/controller/src/MainLoop.cpp
@@ -195,9 +195,8 @@ void tick(const ros::TimerEvent&) {
 
 
             // Put the first behavior on stack
-            TargetHandler::instance()->setHasCube(true);
-            SMACS::instance()->push(new SearchForDropBehavior());
-            SonarHandler::instance()->setEnable(false);
+            SMACS::instance()->push(new SearchBehavior());
+            SonarHandler::instance()->setEnable(true);
 
 
             // Get round type
